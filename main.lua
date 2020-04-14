6884a1fc7fd544a2ed697fc1cd34c8ed3fb82cdc
Alonso Enrique <alons45@gmail.com>
2020-04-13 22:12:29 -0500

    move drawAnimateSquare to figures

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require 'src/figures'
+
 function love.load ()
     -- TODO: move all code to src/figures.lua
     sideSize = 300
@@ -32,42 +34,7 @@ function love.load ()
 end
 
 function love.update ()
-    -- TODO: move all code to src/figures.lua
-    if dynamicX2 < x2 then
-        dynamicX2 = dynamicX2 + increaseSize
-        squareTable = {
-            x1, y1,
-            dynamicX2, y2,
-        }
-
-    elseif dynamicY3 < y3 then
-        dynamicY3 = dynamicY3 + increaseSize
-        squareTable = {
-            x1, y1,
-            dynamicX2, y2,
-            x3, dynamicY3,
-        }
-
-    elseif dynamicX4 > x4 then
-        dynamicX4 = dynamicX4 - increaseSize
-        squareTable = {
-            x1, y1,
-            dynamicX2, y2,
-            x3, dynamicY3,
-            dynamicX4, y4,
-        }
-
-    elseif dynamicY1 > y1 then
-        dynamicY1 = dynamicY1 - increaseSize
-        squareTable = {
-            x1, y1,
-            dynamicX2, y2,
-            x3, dynamicY3,
-            dynamicX4, y4,
-            x1, dynamicY1,
-        }
-
-    end
+    drawAnimateSquare()
 end
 
 function love.draw ()
--- src/figures.lua
+++ src/figures.lua
@@ -1,5 +1,38 @@
 ONE_PERSON_TEXT = 'One Person'
 
-function drawAnimateSquare
+function drawAnimateSquare ()
+    if dynamicX2 < x2 then
+        dynamicX2 = dynamicX2 + increaseSize
+        squareTable = {
+            x1, y1,
+            dynamicX2, y2,
+        }
 
+    elseif dynamicY3 < y3 then
+        dynamicY3 = dynamicY3 + increaseSize
+        squareTable = {
+            x1, y1,
+            dynamicX2, y2,
+            x3, dynamicY3,
+        }
+
+    elseif dynamicX4 > x4 then
+        dynamicX4 = dynamicX4 - increaseSize
+        squareTable = {
+            x1, y1,
+            dynamicX2, y2,
+            x3, dynamicY3,
+            dynamicX4, y4,
+        }
+
+    elseif dynamicY1 > y1 then
+        dynamicY1 = dynamicY1 - increaseSize
+        squareTable = {
+            x1, y1,
+            dynamicX2, y2,
+            x3, dynamicY3,
+            dynamicX4, y4,
+            x1, dynamicY1,
+        }
+    end
 end
