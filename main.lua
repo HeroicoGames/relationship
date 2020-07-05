098970fb36d43faf73259f76f39cccd36e547aac
Alonso <alons45@gmail.com>
2020-07-04 20:22:08 -0500

    hide square when 10 seconds pass

--- main.lua
+++ main.lua
@@ -1,17 +1,27 @@
 local square = require 'src/figures/square'
 local background = require 'src/background'
 
+local deltaTotal = 0
+
 
 function love.load ()
     background.load ()
     square.load (400, 340, 200)
 end
 
-function love.update ()
-    square.update()
+function love.update (deltaTime)
+    deltaTotal = deltaTotal + deltaTime
+
+    if deltaTime < 10 then
+        square.update()
+    end
 end
 
 function love.draw ()
     background.draw ()
-    square.draw()
+    
+    if deltaTotal < 5 then
+        square.draw()
+    end
 end
+
