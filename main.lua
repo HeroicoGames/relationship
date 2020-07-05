cc57d57bf21d9690edbbd9eebd7f89a8438203f0
Alonso <alons45@gmail.com>
2020-07-05 15:34:49 -0500

    add multiple draw for all scenes

--- main.lua
+++ main.lua
@@ -2,68 +2,38 @@ local square = require 'src/figures/square'
 local background = require 'src/background'
 
 local deltaTotal = 0
-local sceneName = ''
+local sceneName = 'one_person'
 
 
 function love.load ()
     background.load ()
-    square.load (400, 340, 200)
 
-    -- Initialize one person
-    -- Initialize two person
-    -- Initialize relationship
-    -- Initialize relationship broken
-    -- Initialize one person support to another
-
-    -- Generate random initial values for each scene
+    square.setInitialValues (200, 150, 200)
 end
 
 function love.update (deltaTime)
     deltaTotal = deltaTotal + deltaTime
 
+    if (deltaTotal < 5 and sceneName ~= 'one_person') then
+        sceneName = 'one_person'
+
+    elseif (deltaTotal > 5 and deltaTotal < 10 and sceneName ~= 'two_people') then
+        sceneName = 'two_people'
+        square.setInitialValues (400, 340, 200)
+
+    elseif (deltaTotal > 10 and deltaTotal < 15 and sceneName ~= 'relationship') then
+        sceneName = 'relationship'
+        square.setInitialValues (500, 240, 200)
+
+    end
+
     square.update()
-
-    --[=====[
-        START
-            if range betwen 0 or 5 seconds
-                sceneName <= one_person
-            else if range between 5 or 10 seconds
-                sceneName <= two_people
-                .
-                .
-                .
-        END
-
-        START
-            if sceneName is one_person
-                updateOnePerson
-            else if sceneName is two_people
-                updateTwoPeople
-                .
-                .
-                .
-        END
-    --]=====]
 
 end
 
 function love.draw ()
     background.draw ()
 
-    if deltaTotal < 5 then
-        square.draw()
-    end
-
-    --[=====[
-        START
-            if sceneName is one_person
-                drawOnePerson
-            else if sceneName is two_people
-                drawTwoPeople
-                .
-                .
-                .
-        END
-    --]=====]
+    square.draw ()
 end
 
